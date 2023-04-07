9c98cefc94ba9aef5c36c9a4cbb68fcb2ec3f652
phuong-nguyen-24 <phuongnguyen241096@gmail.com>
2023-04-07 10:02:10 +0700

    updatefeatures.jsx

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -2,10 +2,149 @@ export default function Features() {
     return (
       <div>
         {/* Container */}
-        <div>
+        <div className="max-w-7xl py-8 px-4 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
           {/* Features Container */}
-          <div>
+          <div className="grid grid-cols-1 gap-y-8 lg:grid-cols-2 lg:gap-x-16 items-center">
               {/* TODO: Create Features Here! */}
+              <div className="text-center max-w-lg mx-auto lg:text-left lg:mx-0">
+                    <h2 className="text-3xl font-bold">Find your career path</h2>
+                    <p className="text-base text-gray-600 mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero aliquid sint distinctio iure ipsum cupiditate? Quis, odit assumenda? Deleniti quasi inventore, libero reiciendis minima aliquid tempora. Obcaecati, autem.</p>
+                    <button className="py-3 px-12 mt-8 text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 rounded inline-block">Get Started Today</button>
+                </div>
+                <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                <div className="p-4 border border-gray-100 hover:border-gray-200 hover:ring-1 hover:ring-gray-200 rounded-xl block shadow-sm">
+                    <div className="inline-block rounded-lg bg-gray-100 p-3">
+                    <svg
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                      />
+                    </svg>
+                    </div>
+                    <h2 className="mt-2 text-inherit font-bold">Accountant</h2>
+                    <p className="hidden text-sm text-gray-600 mt-1 sm:block">Lorem ipsum dolor sit amet consectetur.</p>
+                </div>
+                </div>
           </div>
         </div>
       </div>
