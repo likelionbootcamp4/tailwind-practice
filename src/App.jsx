7d2dc038a240b3159daca727214140ec8ffbfa0c
likelionbootcamp3 <likelionbootcamp4@gmail.com>
2023-04-07 09:56:16 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
+import About from "./components/About/About";
+import Features from "./components/Features/Features";
+import Project from "./components/Project/Project";
+import Specialities from "./components/Specialities/Specialities";
+
 export default function App() {
   return (
     <div>
